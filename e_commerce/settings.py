d8b55f90027ede3c11674c0608daa072db79bd6b
The-vinicius <viniciusnao41@gmail.com>
2021-11-25 20:56:26 +0000

    create and set pages

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,10 +51,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'e_commerce.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR,"template")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
