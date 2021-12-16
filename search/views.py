b810f6383487176b5dda33f073952b0db30822da
The-vinicius <viniciusnao41@gmail.com>
2021-12-16 16:08:02 +0000

    create searchproductview

--- search/views.py
+++ search/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from products.models import Product
+
+class SearchProductView(ListView):
+    template_name = "products/list.html"
+    def get_queryset(self, *args, **kargs):
+        request = self.request
+        return Product.objects.all()
