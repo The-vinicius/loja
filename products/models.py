f37fad510b9b3d196ec4ec805385750541f3d4dc
The-vinicius <viniciusnao41@gmail.com>
2021-12-17 14:19:30 +0000

    query search create in products/models.py

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from .utils import unique_slug_generator
 from django.db.models.signals import pre_save
 from django.urls import reverse
@@ -10,6 +11,10 @@ class ProductQuerySet(models.query.QuerySet):
 
     def featured(self):
         return self.filter(featured = True, active = True)
+
+    def search(self, query):
+        lookups = Q(title__contains = query) | Q(description__contains = query)
+        return self.filter(lookups).distinct()
 
 
 class ProductManager(models.Manager):
@@ -28,6 +33,10 @@ class ProductManager(models.Manager):
         if qs.count() == 1:
             return qs.first()
         return None
+
+    def search(self, query):
+        return self.get_queryset().active().search(query)
+
 
 
 # Create your models here.
