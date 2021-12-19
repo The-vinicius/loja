eabaefcbdd1fa0a0ec6cffbdf054f0ad69f5c207
The-vinicius <viniciusnao41@gmail.com>
2021-12-19 14:40:09 +0000

    typing error

--- carts/models.py
+++ carts/models.py
@@ -9,10 +9,11 @@ class CartManager(models.Manager):
     def new_or_get(self, request):
         cart_id = request.session.get("cart_id", None)
         qs = self.get_queryset().filter(id = cart_id)
-        if qs.count == 1:
+        if qs.count() == 1:
             new_obj = False
+            print(qs)
             cart_obj = qs.first()
-            if request.user.is_authenticate and cart_obj.user is None:
+            if request.user.is_authenticated and cart_obj.user is None:
                 cart_obj.user = request.user
                 cart_obj.save()
         else:
@@ -24,10 +25,9 @@ class CartManager(models.Manager):
     def new(self, user = None):
         user_obj = None
         if user is not None:
-            if user.is_authenticate:
+            if user.is_authenticated:
                 user_obj = user
         return self.model.objects.create(user = user_obj)
-
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
@@ -35,7 +35,10 @@ class Cart(models.Model):
     total = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
     updated = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
+
     objects = CartManager()
 
     def __str__(self):
         return str(self.id)
+
+Product.objects.get_queryset()
