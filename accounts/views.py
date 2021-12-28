7f83eb4978f4b7ec8b4cbf11aec8655ba42bf3ac
The-vinicius <viniciusnao41@gmail.com>
2021-12-28 10:38:46 +0000

    when user is logged delete guest_email_id

--- accounts/views.py
+++ accounts/views.py
@@ -33,22 +33,22 @@ def login_page(request):
                     "form": form
               }
 
-    print("User logged in")
-    print(request.user.is_authenticated)
     next_ = request.GET.get('next')
     next_post = request.POST.get('next')
     redirect_path = next_ or next_post or None
     
     if form.is_valid():
-        print(form.cleaned_data)
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password) 
-        print(user)
-        print(request.user.is_authenticated)
         if user is not None:
-            print(request.user.is_authenticated)
             login(request, user)
+
+            try:
+                del request.session['guest_email_id']
+            except:
+                pass
+
             if url_has_allowed_host_and_scheme( redirect_path, request.get_host() ):
                 return redirect( redirect_path )
             else:
@@ -73,10 +73,8 @@ def register_page(request):
                     "form": form
               }
     if form.is_valid():
-        print(form.cleaned_data)
         username = form.cleaned_data.get("username")
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
         new_user = User.objects.create_user(username, email, password)
-        print(new_user)
     return render(request, "accounts/register.html", context) 
